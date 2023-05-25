df36a0a701f6463844ca6130a964c4a1ce6bcb44
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2023-05-25 22:08:23 +0500

    feat: added relations between models, update contoller and views

--- app/Category.php
+++ app/Category.php
@@ -4,7 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
-{
-    //
+class Category extends Model {
+    /**
+     * Возвращает список товаров выбранной категории
+     */
+    public function getProducts() {
+        return Product::where('category_id', $this->id)->get();
+    }
+    /**
+     * Связь «один ко многим» таблицы `categories` с таблицей `products`
+     */
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -4,7 +4,30 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
-{
-    //
+class Product extends Model {
+    /**
+     * Возвращает категорию выбранного товара
+     */
+    public function getCategory() {
+        return Category::find($this->category_id);
+    }
+    /**
+     * Возвращает бренд выбранного товара
+     */
+    public function getBrand() {
+        return Brand::find($this->brand_id);
+    }
+    /**
+     * Связь «товар принадлежит» таблицы `products` с таблицей `categories`
+     */
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Связь «товар принадлежит» таблицы `products` с таблицей `brands`
+     */
+    public function brand() {
+        return $this->belongsTo(Brand::class);
+    }
 }
