1517f14b15f69d3c4a1a3c790a85ba17819a8b97
Anna Kolesnikova <ankolesnikova@tourmalinecore.com>
2023-05-25 12:42:12 -0700

    first commit

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeeder extends Seeder {
+    public function run() {
+        // создать 4 категории
+        factory(App\Category::class, 4)->create();
+    }
+}
